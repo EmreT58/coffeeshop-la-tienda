1c5ca9184b9db92aad490f24edabd06b1cd99798
EmreT58 <emre.taskopru@hva.nl>
2023-12-20 21:55:33 +0100

    beginnetje met area mapping op image

---
+++ script.js
@@ -0,0 +1,7 @@
+function goInside() {
+    // Hide the outside image
+    document.getElementById('outsideImage').style.display = 'none';
+    
+    // Show the inside image
+    document.getElementById('insideImage').style.display = 'block';
+}
