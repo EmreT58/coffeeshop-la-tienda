6c9489133417362c5bdad44aeac44e528d978839
EmreT58 <45204376+EmreT58@users.noreply.github.com>
2024-01-24 17:29:23 +0100

    0.9.1 javascript typewriter toegevoegd
    duurde 2/3 uurtjes om een goede te vinden en het daadwerkelijk werkend te krijgen :/ Nu verder

--- script.js
+++ script.js
@@ -1,8 +1,66 @@
 // https://github.com/anvaka/panzoom
 
 window.onload = function() { 
-  zoomer.smoothMoveTo(-800, -200); // smooth scroll naar midden
+  zoomer.smoothMoveTo(-900, -350); // smooth scroll naar midden
 };
+
+// ===-TYPEWRITER EFFECT FOR INTRO VIDEO -===
+
+// code gebruikt van timdotcoce https://codepen.io/timdotcode/pen/bOejXp
+var typeWriterElement = document.getElementById('typewriter');
+
+var textArray = [
+  "Colombia is known for it’s rich & diverse culture and for their lively society.",
+  "Bakeries play a big part in the lives of these Colombian citizens. As they don’t just use it to get bread.", 
+  "Let's explore!"
+];
+
+// function to generate the backspace effect 
+function delWriter(text, i, cb) {
+	if (i >= 0 ) {
+		typeWriterElement.innerHTML = text.substring(0, i--);
+		// generate a random Number to emulate backspace hitting.
+ 		var rndBack = 10 + Math.random() * 100;
+		setTimeout(function() {
+			delWriter(text, i, cb);
+		},rndBack); 
+	} else if (typeof cb == 'function') {
+		setTimeout(cb,1000);
+	}
+};
+
+// function to generate the keyhitting effect
+function typeWriter(text, i, cb) {
+	if ( i < text.length+1 ) {
+		typeWriterElement.innerHTML = text.substring(0, i++);
+		// generate a random Number to emulate Typing on the Keyboard.
+		var rndTyping = 165 - Math.random() * 100;
+		setTimeout( function () { 
+			typeWriter(text, i++, cb)
+		},rndTyping);
+	} else if (i === text.length+1) {
+		setTimeout( function () {
+			delWriter(text, i, cb)
+		},1000);
+	}
+};
+
+// the main writer function
+function StartWriter(i) {
+	if (typeof textArray[i] == "undefined") {
+		setTimeout( function () {
+			StartWriter(0)
+		},1000);
+	} else if(i < textArray[i].length+1) {
+		typeWriter(textArray[i], 0, function () {
+			StartWriter(i+1);
+		});
+	}  
+};
+
+
+
+// ===- END OF TYPEWRITER -===
 
 var zoomer = panzoom(document.querySelector("#image-container"), {
   maxZoom: 20,
@@ -89,7 +147,13 @@ function playVideo() {
   var video = document.getElementById("introvideo");
   video.play();
   document.getElementById('video-overlay').style.opacity = '0';
+  document.getElementById('typewriter-container').style.opacity = '1';
   document.getElementById('skipvideo').style.display = 'block';
+
+  // wait one second then start the typewriter
+  setTimeout( function () {
+	  StartWriter(0);
+  },1000);
 }
 
 // Fade Out Function
@@ -141,5 +205,6 @@ function playBalie() {
   // Ik zou hier kunnen toevoegen om video te pauzeren als de opacity weer 0 wordt
   // Maar aanhouden kan ook leuk zijn als achtergrond geluid, zolang er geen conflict is
 }
+
 
 
